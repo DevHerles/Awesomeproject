9af963e4fa8b60885773f41c9776bdfb2bad5753
DevHerles <=>
2019-04-15 18:33:27 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+} from 'react-native';
+
+import Login from './Login';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          backgroundColor="#1c313a"
+          barStyle="light-content"
+        />
+        <Login/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#455a64',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Login.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
+
+import Logo from './Logo';
+import Form from './Form';
+
+export default class Login extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Logo/>
+        <Form/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#455a64',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
